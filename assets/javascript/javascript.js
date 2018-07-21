d1aedb65657819ac5d37c4ac82efc6bee672ffaf
chefgonzo <chefgonzo@gmail.com>
2018-07-21 12:18:41 -0400

    javascript pseudocoded

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,13 @@
+//Setting the game up to run when the window has finshed loading
+$(document).ready(function(){
+    
+    //Global Variables
+    //reset function
+    //Genreate a random taret score
+    //generate a random value for each crysal
+    //player clicks a crystal and score is updated
+    //win
+    //loss
+    //update wins and losses
+    //start over
+})
