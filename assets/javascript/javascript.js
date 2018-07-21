90e6f008494dba1ae060aee52970324a7a7271cb
chefgonzo <chefgonzo@gmail.com>
2018-07-21 15:29:10 -0400

    reached problem in the JS

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -15,21 +15,50 @@ $(document).ready(function(){
     
     //Generate a random value for each Crystal
     function crystalValues() {
-    var redCrystalValue = Math.floor(Math.random() * 11 + 1);
-    console.log("Red Crystal Value = " + redCrystalValue);
-    var blackCrystalValue = Math.floor(Math.random() * 11 + 1);
-    console.log("Black Crystal Value = " + blackCrystalValue);
-    var yellowCrystalValue = Math.floor(Math.random() * 11 + 1);
-    console.log("yellow Crystal Value = " + yellowCrystalValue);
-    var purpleCrystalValue = Math.floor(Math.random() * 11 + 1);
-    console.log("Purple Crystal Value = " + purpleCrystalValue);
+        var redCrystalValue = Math.floor(Math.random() * 11 + 1);
+        console.log("Red Crystal Value = " + redCrystalValue);
+        return redCrystalValue;
+        var blackCrystalValue = Math.floor(Math.random() * 11 + 1);
+        console.log("Black Crystal Value = " + blackCrystalValue);
+        return blackCrystalValue;
+        var yellowCrystalValue = Math.floor(Math.random() * 11 + 1);
+        console.log("yellow Crystal Value = " + yellowCrystalValue);
+        return yellowCrystalValue;
+        var purpleCrystalValue = Math.floor(Math.random() * 11 + 1);
+        console.log("Purple Crystal Value = " + purpleCrystalValue);
+        return purpleCrystalValue;
     }
+    //RE-Assigning Crystal Values to new variables outside the function
+    var redValue = redCrystalValue;
+    var blackValue = blackCrystalValue;
+    var yellowValue = yellowCrystalValue;
+    var purpleValue = purpleCrystalValue;
 
     //Display wins and losses
     $("#wins").text(wins);
     $("#losses").text(losses);
     
     //player clicks a crystal and score is updated
+    $("#buttonred").on("click", function() {
+        totalScore = totalScore + redValue;
+        $("#totalscore").text(totalScore);
+        console.log("Total Score is : " + totalScore);
+    })
+    $("#buttonblack").on("click" , function() {
+        totalScore = totalScore + blackValue;
+        $("#totalscore").text(totalScore);
+        console.log("Total Score is : " + totalScore);
+    })
+    $("#buttonyellow").on("click" , function () {
+        totalscore = totalScore + yellowValue;
+        $("#totalscore").text(totalScore);
+        console.log("Total Score is : " + totalScore);
+    })
+    $("#buttonpurple").on("click" , function() {
+        totalScore = totalScore + purpleValue;
+        $("#totalscore").text(totalScore);
+        console.log("Total Score is : " + totalScore);
+    })
 
     //win
     //loss
