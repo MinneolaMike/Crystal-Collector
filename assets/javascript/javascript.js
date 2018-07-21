8d7e887f11c994cfa11d3b8dd1aaea7e26833a6b
chefgonzo <chefgonzo@gmail.com>
2018-07-21 12:51:25 -0400

    JS - all random numbers are being genreated properly

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -2,12 +2,37 @@
 $(document).ready(function(){
     
     //Global Variables
-    //reset function
-    //Genreate a random taret score
-    //generate a random value for each crysal
+    var totalScore = 0;
+    var wins = 0;
+    var losses = 0;
+
+    //Genreate a random Target Score
+    var targetScore = Math.floor(Math.random() * 101 + 19);
+    console.log("The target score is = " + targetScore);
+    //Display it in the HTML
+    $("#targetscore").text(targetScore);
+    crystalValues();
+    
+    //Generate a random value for each Crystal
+    function crystalValues() {
+    var redCrystalValue = Math.floor(Math.random() * 11 + 1);
+    console.log("Red Crystal Value = " + redCrystalValue);
+    var blackCrystalValue = Math.floor(Math.random() * 11 + 1);
+    console.log("Black Crystal Value = " + blackCrystalValue);
+    var yellowCrystalValue = Math.floor(Math.random() * 11 + 1);
+    console.log("yellow Crystal Value = " + yellowCrystalValue);
+    var purpleCrystalValue = Math.floor(Math.random() * 11 + 1);
+    console.log("Purple Crystal Value = " + purpleCrystalValue);
+    }
+
+    //Display wins and losses
+    $("#wins").text(wins);
+    $("#losses").text(losses);
+    
     //player clicks a crystal and score is updated
+
     //win
     //loss
     //update wins and losses
-    //start over
+    //reset
 })
